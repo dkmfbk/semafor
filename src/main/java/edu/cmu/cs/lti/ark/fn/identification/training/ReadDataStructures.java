564e18c3c8bf94e52e3133edc523851eab87fc8b
Alessio Palmero Aprosio <alessio@apnetwork.it>
2015-12-23 11:29:51 +0100

    Classes for creation of DataStructure

---
+++ src/main/java/edu/cmu/cs/lti/ark/fn/identification/training/ReadDataStructures.java
@@ -0,0 +1,14 @@
+package edu.cmu.cs.lti.ark.fn.identification.training;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by alessio on 22/12/15.
+ */
+
+public class ReadDataStructures {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReadDataStructures.class);
+
+}
